d0915d3fb5ac85bd3af76b70d77f0c6f39588977
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-06-10 21:50:56 -0400

    adding the constants file

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 const { Socket } = require("dgram");
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // Code below establishes a connection with the game server
 
 const connect = function () {
   const conn = net.createConnection({
-    host: "10.0.0.211",
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
 // The code below interprets incoming data
---
+++ constants.js
@@ -0,0 +1,8 @@
+const IP = "10.0.0.211";
+const PORT = 50541
+
+
+module.exports = {
+  IP,
+  PORT
+};
