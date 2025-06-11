1b9ed79a692672088c9664e4fe18964b3d2f445e
Daniela Pachon <[pachon.daniela@gmail.com]>
2025-06-10 20:12:48 -0400

    adding the input file

--- play.js
+++ play.js
@@ -1,27 +1,9 @@
-const connect = require("./client.js")
+const connect = require("./client.js");
 
-const setupInput = function () {
-  const stdin = process.stdin; // create variable to hold the stdin object so we don't have to type process.stdin multiple times
+const setupInput = require("./input.js");
 
-  stdin.setRawMode(true); // Raw Mode allows us to listen for individual keypresses instead of waiting for the user to press enter
-
-  stdin.setEncoding("utf8"); // utf8 encoding is set so that we can read the text data that is input
-
-  stdin.resume(); // resume stdin so the program can listen for input
-
-  stdin.on("data", handleUserInput); //You'll now register an event listener for stdin. In the handleUserInput function, you'll specify what happens when "data" is received from stdin... in other words, what happens when a particular key is pressed on the keyboard input.
-
-  return stdin; // return the stdin object so we can use it elsewhere in the program
-};
-
-const handleUserInput = function (key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
-
+console.log("Connecting...");
 connect();
-setupInput();
 
-
+setupInput()
 
